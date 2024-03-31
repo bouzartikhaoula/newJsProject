b0d29fe8efce3d3c7b696991786c71018fadbe70
khaoula <bouzartikhaoula@gmail.com>
2024-03-31 10:24:02 +0200

    last add

--- main.js
+++ main.js
@@ -21,8 +21,8 @@ logoDiv.className = "logo";
 menuDiv.className = "menu";
 
 // stayle
-headerel.style.cssText=(" margin: 0px 35px;display: flex;justify-content: space-between;align-items: center")
-menuDiv.style.cssText=("    display: flex;     width: 251px;     justify-content: space-between;align-items: center;")
+headerel.style.cssText = ("padding: 8px 5%;display: flex;justify-content: space-between;align-items: center;background-color: white;")
+menuDiv.style.cssText = ("    display: flex;     width: 251px; justify-content: space-between;align-items: center;")
 ilElement1.style.listStyleType = ("none");
 ilElement2.style.listStyleType = ("none");
 ilElement3.style.listStyleType = ("none");
@@ -49,5 +49,41 @@ ilElement3.appendChild(text3);
 menuDiv.appendChild(ilElement4);
 ilElement4.appendChild(text4);
 
-
 document.body.appendChild(headerel)
+
+// section2
+
+
+
+
+
+
+
+let divContent = document.createElement("div")
+divContent.className = ("Content")
+
+for (let i = 1; i < 16; i++) {
+    let divProdect = document.createElement("div")
+    let spanProdect = document.createElement("span")
+    let spanContent = document.createTextNode(`${i}`)
+    let ProdectContent = document.createTextNode("prodect")
+    divProdect.className = ("Prodect")
+    spanProdect.appendChild(spanContent)
+    divProdect.appendChild(spanProdect)
+    divContent.appendChild(divProdect)
+    divProdect.appendChild(ProdectContent)
+    divProdect.style.cssText = ("display: grid; justify-content: center;align-items: center;width: 100%;height: 80px;text-align: center;color: rgba(5, 137, 5, 0.92);background-color: white;")
+    spanProdect.style.cssText = ("font-family: cursive;color: black;text-align: center;font-size: x-large;")
+
+}
+
+// style section 2
+
+divContent.style.cssText = ("display:grid ;grid-template-columns: 1fr 1fr 1fr;padding: 10px;justify-content: space-between;align-items: center;justify-items: center;gap: 20px; width: 80%;margin: auto;")
+document.body.style.cssText = ("background-color: #bdbdbd2b;margin: 0;")
+
+
+document.body.appendChild(divContent)
+
+
+
